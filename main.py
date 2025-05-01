c4a766072ae158a2a6cd00af0bf9140b70e3f6f6
hjelp200121 <johan.augsburg@gmail.com>
2025-05-01 11:43:44 +0200

    idk

--- main.py
+++ main.py
@@ -1,45 +1,18 @@
-import matplotlib.pyplot as plt
 import pandas as pd
-import numpy as np
-import sklearn.cluster as skc
+from sklearn.cluster import KMeans
 
-def histogram_selected_features():
-    # histograms of selected features
-    fig, axes = plt.subplots(1, 5, figsize=(14, 3))
-    plt.subplots_adjust(wspace=0.1, left=0.01, right=0.99)
-
-    con_features = [11, 19, 65, 80, 91]
-
-    for i, feature in enumerate(con_features):
-        axes[i].hist(X[:, feature], bins=10, color="#440154FF")
-        axes[i].set_xticks([])
-        axes[i].set_yticks([])
-        axes[i].set_title(f"Feature x_{feature}")
-
-    plt.savefig("figures/5_features_hist.pdf")
-
-def correlation_analysis():
-    # correlation analysis
-    corr_mat = df[df.columns[1:96]].corr()
-    T = np.triu(corr_mat.to_numpy(), 1)
-    n = T.shape[0]
-
-    print(np.sum(T > 0.5))
-
-    plt.imshow(corr_mat)
-    plt.colorbar()
-    plt.xticks([])
-    plt.yticks([])
-    plt.savefig("figures/corr_matrix.pdf")
-
-# load data
 data_path = "data/HR_data_cleaned.csv"
 df = pd.read_csv(data_path)
 
-X = np.array(df[df.columns[1:51]])
-N, p = X.shape
+X = df.iloc[:, 1:51]
+columns_to_average = list(df.columns[[55]]) + list(df.columns[57:67])
+Y = df[columns_to_average]
 
-kmeans = skc.KMeans(n_clusters=5).fit(X)
-mask = kmeans.labels_
+n_clusters = 5
+kmeans = KMeans(n_clusters=n_clusters)
+clusters = kmeans.fit_predict(X)
 
-for i in range(10):
+df['cluster'] = clusters
+cluster_means = df.groupby('cluster')[columns_to_average].mean()
+
+print(cluster_means)
